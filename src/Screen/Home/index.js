9b1ee1bbca118235338a6a1874aceadae2b523c7
fajar <fajarfathirwan97@gmail.com>
2018-08-28 23:18:16 +0700

    add title,content

--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, View, Image, ImageBackground } from 'react-native';
-import { Content, Container, Text, Icon } from "native-base";
+import { Content, Container, Text, Icon, H3, Card, CardItem } from "native-base";
 import { Metrics } from '../../Style';
 
 type Props = {};
@@ -20,28 +20,42 @@ export default class Home extends Component<Props> {
     return (
       <Container style={styles.container}>
         <Content>
-          <ImageBackground
-            resizeMethod='resize'
-            resizeMode='stretch'
-            style={styles.imageBackground}
-            source={{ uri: 'https://awsimages.detik.net.id/community/media/visual/2017/05/31/d4e24277-0094-4555-b069-58e354d30f8f_169.jpg?w=780&q=90' }} >
-            <View style={styles.containerImageBackground}>
-              <View style={styles.wrapperContainerImageBackground}>
-                <View style={[styles.sectionImageBackground, { flex: 3, paddingLeft: 10 }]}>
-                  <Text style={styles.imageBackgroundText}>POLITIK</Text>
-                </View>
-                <View style={[styles.sectionImageBackground, { flexDirection: 'row', paddingRight: 10, flex: 7, justifyContent: 'flex-start' }]}>
-                  <Icon type='Ionicons' name='time' style={[styles.imageBackgroundText, { paddingRight: 10 }]} />
-                  <Text style={[styles.imageBackgroundText]}>
-                    Baru Saja
+          <View style={{ justifyContent: 'space-around', flex: 0, borderBottomColor: 'grey', borderBottomWidth: 1 }}>
+            <ImageBackground
+              resizeMethod='resize'
+              resizeMode='stretch'
+              style={styles.imageBackground}
+              source={{ uri: 'https://awsimages.detik.net.id/community/media/visual/2017/05/31/d4e24277-0094-4555-b069-58e354d30f8f_169.jpg?w=780&q=90' }} >
+              <View style={styles.containerImageBackground}>
+                <View style={styles.wrapperContainerImageBackground}>
+                  <View style={[styles.sectionImageBackground, { flex: 3, paddingLeft: 10 }]}>
+                    <Text style={styles.imageBackgroundText}>POLITIK</Text>
+                  </View>
+                  <View style={[styles.sectionImageBackground, { flexDirection: 'row', paddingRight: 10, flex: 7, justifyContent: 'flex-start' }]}>
+                    <Icon type='Ionicons' name='time' style={[styles.imageBackgroundText, { paddingRight: 10 }]} />
+                    <Text style={[styles.imageBackgroundText]}>
+                      Baru Saja
                   </Text>
-                </View>
-                <View style={[styles.sectionImageBackground, { flex: 2 }]}>
-                  <Icon type='MaterialIcons' name='bookmark' style={styles.imageBackgroundText} />
+                  </View>
+                  <View style={[styles.sectionImageBackground, { flex: 2 }]}>
+                    <Icon type='MaterialIcons' name='bookmark' style={styles.imageBackgroundText} />
+                  </View>
                 </View>
               </View>
+            </ImageBackground>
+            <Card transparent>
+              <CardItem>
+                <Text style={{ fontSize: Metrics.screenHeight * 0.05 }}>Membaca Sinyal Dari Istana, Ini Cawapres di Saku Jokowi</Text>
+              </CardItem>
+              <CardItem>
+                <Text note style={{ fontSize: Metrics.screenHeight * 0.025 }}>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </Text>
+              </CardItem>
+            </Card>
+            <View style={{ flex: 1 }}>
             </View>
-          </ImageBackground>
+          </View>
         </Content>
       </Container>
     );
